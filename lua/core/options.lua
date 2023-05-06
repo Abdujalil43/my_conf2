dae1e638c3167d6ebbe1f5c5b440a98c6d3fe49b
abdujalil <abdujalilabdurakhmonov9@gmail.com>
2023-05-06 21:38:24 +0500

    cmdheight added

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -7,3 +7,4 @@ vim.opt.number = true				-- number
 vim.opt.relativenumber = true			-- relative number
 vim.opt.termguicolors = true			-- allows the use of true color (24-bit color) in the terminal
 vim.opt.showmode = false			-- controls whether or not the current mode
+vim.opt.cmdheight = 1				-- controls the height of the command-line window
