17b82cb1340b798288d85ffdc1dc27d7fcee64f5
abdujalil <abdujalilabdurakhmonov9@gmail.com>
2023-05-06 21:52:30 +0500

    tabline/

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,3 +8,5 @@ vim.opt.relativenumber = true			-- relative number
 vim.opt.termguicolors = true			-- allows the use of true color (24-bit color) in the terminal
 vim.opt.showmode = false			-- controls whether or not the current mode
 vim.opt.cmdheight = 1				-- controls the height of the command-line window
+vim.opt.pumheight = 10				-- appears when you use auto-completion or other dropdown menus
+vim.opt.showtabline = 2				-- controls the display of the tabline
