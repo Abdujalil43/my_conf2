858c625b2810ff175cee7453fa247d16002c84d7
abdujalil <abdujalilabdurakhmonov9@gmail.com>
2023-05-06 21:26:10 +0500

    changed options.lua

---
+++ lua/core/options.lua
@@ -0,0 +1,9 @@
+
+
+-- ==========================================================================
+-- UI
+-- ==========================================================================
+vim.opt.number = true				-- number
+vim.opt.relativenumber = true			-- relative number
+vim.opt.termguicolors = true			-- allows the use of true color (24-bit color) in the terminal
+vim.opt.showmode = false			-- controls whether or not the current mode
